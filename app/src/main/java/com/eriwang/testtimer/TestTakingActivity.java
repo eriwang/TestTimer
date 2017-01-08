0705988e4f9d5743727cf0db0fa59183bb73ab24
Eric Wang <johnericcw@gmail.com>
2017-01-07 23:21:07 -0500

    Add button for next question

--- app/src/main/java/com/eriwang/testtimer/TestTakingActivity.java
+++ app/src/main/java/com/eriwang/testtimer/TestTakingActivity.java
@@ -2,28 +2,56 @@ package com.eriwang.testtimer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 import com.eriwang.testtimer.timer.QuestionCountUpTimer;
 import com.eriwang.testtimer.timer.TestCountdownTimer;
 
 public class TestTakingActivity extends AppCompatActivity {
 
-	TestCountdownTimer fullTestTimer;
-	QuestionCountUpTimer questionTimer;
+	private TestCountdownTimer fullTestTimer;
+	private QuestionCountUpTimer questionTimer;
+	private int currentQuestionNumber;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_test_taking);
+		currentQuestionNumber = 1;
 
+		fullTestTimer = null;
+		questionTimer = null;
+
+		startFullTestTimer();
+		startQuestionTimer();
+		setQuestionNumber(currentQuestionNumber);
+	}
+
+	public void nextQuestion(View view) {
+		++currentQuestionNumber;
+		setQuestionNumber(currentQuestionNumber);
+		startQuestionTimer();
+	}
+
+	private void startFullTestTimer() {
 		TextView fullTestTimerTextView = (TextView) findViewById(R.id.fullTestTimer);
 		fullTestTimer = new TestCountdownTimer(60 * 1000, 1000, fullTestTimerTextView);
+		fullTestTimer.start();
+	}
+
+	private void startQuestionTimer() {
+		if (questionTimer != null) {
+			questionTimer.cancel();
+		}
 
 		TextView questionTimerTextView = (TextView) findViewById(R.id.questionTimer);
 		questionTimer = new QuestionCountUpTimer(60 * 1000, 1000, questionTimerTextView);
-
-		fullTestTimer.start();
 		questionTimer.start();
+	}
+
+	private void setQuestionNumber(int questionNumber) {
+		TextView questionNumberTextView = (TextView) findViewById(R.id.questionNumber);
+		questionNumberTextView.setText(String.valueOf(questionNumber));
 	}
 
 }
